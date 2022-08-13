9231e5901073cca763077ac832f1349870ef1fde
rayhayes <ray.hayes@gmail.com>
2022-08-13 15:26:40 +0100

    Add some javascript to manipulate the DOM to remove the style of .splash

---
+++ BlazorWasmSplash/wwwroot/js/splash.js
@@ -0,0 +1,8 @@
+﻿export function toggleSplashClass() {
+    if ( document.documentElement.classList.contains('splash') ) {
+        document.documentElement.classList.remove('splash');
+    }
+    else {
+        document.documentElement.classList.add('splash');
+    }
+}
