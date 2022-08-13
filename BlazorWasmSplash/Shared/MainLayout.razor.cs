26e119273a3d0c989cae3f1b6d10ecbc16b2bce4
rayhayes <ray.hayes@gmail.com>
2022-08-13 15:27:51 +0100

    Remove splash class reference when Blazor is loaded
    Invoke JavaScript when first rendering the MainLayout and toggle off the .splash
    class from HTML.

---
+++ BlazorWasmSplash/Shared/MainLayout.razor.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazorWasmSplash.Shared;
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+public partial class MainLayout
+{
+    [Inject]
+    private IJSRuntime JsRuntime { get; set; } = default!;
+    
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            await using var splashModule = await JsRuntime.InvokeAsync<IJSObjectReference>("import", "/js/splash.js");
+            await splashModule.InvokeVoidAsync("toggleSplashClass");
+
+        }
+    }
+}
